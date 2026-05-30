f9210562a7c73dcbb39fbfb3e75f0a7d0a2e585c
sairaj724 <sairajdesai724@gmail.com>
2026-05-30 06:59:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11 MB (7.46%)

---
+++ 0009-palindrome-number/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        return rev == s;
+
+        
+    }
+};
