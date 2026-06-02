b537f86894b3198efc5578ebda7caae4d9150747
sairaj724 <sairajdesai724@gmail.com>
2026-06-02 13:05:03 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 92.9 MB (5.16%)

---
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    int solve(vector<int>& start1, vector<int>& dur1, vector<int>& start2, vector<int> dur2){
+        int finish1 = INT_MAX;
+        for(int i = 0; i< start1.size(); i++){
+            finish1 = min(finish1 , start1[i]+ dur1[i]);
+        }
+
+        int finish2 = INT_MAX;
+        for(int i = 0; i < start2.size(); i++){
+            finish2 = min(finish2, max(start2[i],finish1)+dur2[i]);
+        }
+        return finish2;
+    }
+
+public:
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int land_water =
+            solve(landStartTime, landDuration, waterStartTime, waterDuration);
+        int water_land =
+            solve(waterStartTime, waterDuration, landStartTime, landDuration);
+        return min(land_water, water_land);
+    }
+};
