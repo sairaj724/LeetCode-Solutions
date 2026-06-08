edda608c39003568ff8a9728dc540baa6b22b138
sairaj724 <sairajdesai724@gmail.com>
2026-06-08 06:04:33 +0000

    Sync LeetCode submission Runtime - 1 ms (91.93%), Memory - 127.6 MB (88.34%)

---
+++ 2265-partition-array-according-to-given-pivot/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        int n = nums.size();
+        vector<int> ans(n,pivot);
+
+        int left = 0;
+        int right = n-1;
+
+        for(int i =0, j= n-1; i<n ; i++,j--){
+            if(nums[i] < pivot){
+                ans[left] = nums[i];
+                left++;
+            }
+
+            if(nums[j] > pivot){
+                ans[right] = nums[j];
+                right--;
+            }
+        }
+        
+        return ans;
+    }
+};
