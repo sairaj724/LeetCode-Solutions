f838b21eed363341f670fcec8e53fa3ddc44b72c
sairaj724 <sairajdesai724@gmail.com>
2026-06-08 11:59:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.7 MB (23.24%)

---
+++ 0941-sort-array-by-parity/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0;
+        int right = n-1;
+
+        while(left < right){
+            if(nums[left] % 2 == 0){
+                left++;
+            }
+            else if(nums[right] % 2 != 0){
+                right--;
+            }
+            else{
+                swap(nums[left] , nums[right]);
+                right--;
+                left++;
+            }
+        }
+        return nums;
+    }
+};
