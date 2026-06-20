93c11486e0ef6090a58515026090fbcc2a920f5c
sairaj724 <sairajdesai724@gmail.com>
2026-06-20 14:14:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (13.23%)

---
+++ 2507-number-of-common-factors/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+       int gcdValue = gcd(a, b);
+        int count = 0;
+        
+        for (int i = 1; i * i <= gcdValue; i++) {
+            if (gcdValue % i == 0) {
+                count++;
+                if (i * i != gcdValue) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
