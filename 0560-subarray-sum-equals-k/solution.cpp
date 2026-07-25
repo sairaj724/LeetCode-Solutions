2c5c66993e148dc0405dda39fdd9b4fab874c5ea
sairaj724 <sairajdesai724@gmail.com>
2026-07-25 11:44:44 +0000

    Sync LeetCode submission Runtime - 42 ms (61.07%), Memory - 46.8 MB (35.53%)

---
+++ 0560-subarray-sum-equals-k/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+        vector<int> prefixSum(n,0);
+
+        prefixSum[0] = nums[0];
+        for(int i =1; i<n; i++){
+            prefixSum[i] = nums[i] + prefixSum[i-1];
+        }
+         
+        unordered_map<int,int> m;
+
+        for(int j =0; j<n; j++){
+            if(prefixSum[j] == k) count++;
+
+            int val = prefixSum[j] - k;
+            if(m.find(val) != m.end()){
+                count += m[val];
+            }
+
+            
+            m[prefixSum[j]]++;   
+        }
+        return count;
+    }
+};
